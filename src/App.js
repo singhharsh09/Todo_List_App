50d0fa213e9b07bed792abba8006ff6570dcd0cb
singhharsh09 <101403694+singhharsh09@users.noreply.github.com>
2023-11-17 04:14:24 +0530

    all changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import Header from "./MyComponents/header";
+import {Todos} from "./MyComponents/Todos";
+import {TodoItem} from "./MyComponents/TodoItem";
+import {Footer} from "./MyComponents/Footer";
+import {AddTodo} from "./MyComponents/AddTodo";
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+import Home from './MyComponents/Home';
+import About from './MyComponents/About';
 
 function App() {
+  // const[ todos,setTodos]=useState([ ])
+  // // let initTodo;
+  // // if(localStorage.getItem("todos")){
+    
+  // //   initTodo=[];
+
+  // // }
+  // // else {
+  // //   initTodo=JSON.parse(localStorage.getItem("todos"));
+  // // }
+  // const onDelete=(todo)=>{
+  //   console.log("I am deleted",todo);
+
+  //   setTodos(todos.filter((e)=>{
+  //     return e!==todo;
+  //   }))
+  //  // localStorage.setItem("todos",JSON.stringify(todos));
+  // }
+  // const addTodo=(title,desc)=>{
+  //      let id=todos.length;
+  //      console.log("adding todo",id,title,desc);
+  //      const myTodo={
+  //       id:id+1,
+  //       title:title,
+  //       desc:desc
+  //      }
+  //      setTodos([...todos,myTodo]);
+       
+  //       //localStorage.setItem("todos",JSON.stringify(todos));
+      
+  // }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <BrowserRouter>
+   <Home/>
+   <Routes>
+    {/* <Route path='/' element=<Header/>></Route> */}
+    <Route path='/about' element=<About/>> </Route>
+   
+   
+   </Routes>
+ 
+   </BrowserRouter>
+  )
 }
+
 
 export default App;
